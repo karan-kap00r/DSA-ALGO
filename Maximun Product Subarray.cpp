9755c043c8389f3cb18d5ff51ab68a5bb446f314
karan kapoor <kaykay9464769@gmail.com>
2022-07-08 23:42:03 +0530

    Arrays

---
+++ Maximun Product Subarray.cpp
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	// Function to find maximum product subarray
+long long maxProduct(vector<int> arr, int n)
+ {
+    long long res=arr[0],ma=1,mi=1;
+    for(auto i:arr)
+    {
+        long long t=i*ma;
+        ma=max(t,max(i*mi,(long long)i));
+        mi=min(t,min(i*mi,(long long)i));
+        res=max(res,ma);
+    }
+    return res;
+}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
